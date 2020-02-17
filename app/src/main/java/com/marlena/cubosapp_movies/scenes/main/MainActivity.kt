865721ade95ad8ef6c4b075ed8847939bd4dbaea
MarlenaMartins1533 <marlenamotamartins@gmail.com>
2020-02-17 15:48:58 -0300

    Search done

--- app/src/main/java/com/marlena/cubosapp_movies/scenes/main/MainActivity.kt
+++ app/src/main/java/com/marlena/cubosapp_movies/scenes/main/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.marlena.cubosapp_movies.scenes.main
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.isVisible
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -13,8 +13,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 import com.marlena.cubosapp_movies.R
-import com.marlena.cubosapp_movies.scenes.search.SearchFragment
-import com.marlena.cubosapp_movies.scenes.search.SearchFragment.Companion.SEARCHTEXT_ARG
+import com.marlena.cubosapp_movies.scenes.search.SearchActivity
 import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity(), Main.View {
@@ -46,14 +45,13 @@ class MainActivity : AppCompatActivity(), Main.View {
 
     private fun initListener() {
         searchBTN.setOnClickListener {
-            if (searchEDT.isVisible) {
-                getEDTsOpenSearch()
-            } else searchEDT.visibility = View.VISIBLE
+                goToSearch()
         }
     }
 
-    private fun getEDTsOpenSearch() {
-        SearchFragment.newInstance(searchEDT.text.toString())
+    private fun goToSearch() {
+        val intent = Intent(this, SearchActivity::class.java)
+        startActivity(intent)
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/com/marlena/cubosapp_movies/scenes/search/SearchActivity.kt
+++ app/src/main/java/com/marlena/cubosapp_movies/scenes/search/SearchActivity.kt
@@ -5,58 +5,69 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Pair
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
 import com.marlena.cubosapp_movies.R
 import com.marlena.cubosapp_movies.core.App
 import com.marlena.cubosapp_movies.model.domain.Movie
 import com.marlena.cubosapp_movies.scenes.adapter.MovieAdapter
 import com.marlena.cubosapp_movies.scenes.theMovie.TheMovieActivity
-import kotlinx.android.synthetic.main.fragment_movie_list.*
+import kotlinx.android.synthetic.main.activity_search.*
+import kotlinx.android.synthetic.main.activity_search.searchBTN
+import kotlinx.android.synthetic.main.activity_search.searchEDT
+import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.fragment_movie_list.recyclerViewRV
 
-class SearchFragment : Fragment(), Search.View, MovieAdapter.Listener {
+class SearchActivity : AppCompatActivity(), Search.View, MovieAdapter.Listener {
     private var searchText: String? = " "
     private lateinit var movieList: List<Movie>
     private val resultList = mutableListOf<Movie>()
     private var adapter: MovieAdapter? = null
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        searchText = arguments?.getString(SEARCHTEXT_ARG) ?: " "
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+
+        setSupportActionBar(toolbarSearch)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+//        searchText = intent.getStringExtra(SEARCH_ARG) ?: " "
+//         = searchText
+        initListener()
         getMoviesList()
         onSearchSubmit()
-        return inflater.inflate(R.layout.fragment_movie_list, container, false)
     }
 
     private fun onSearchSubmit() {
-        val text = searchText
+        val text = searchEDT.text.toString()
+        searchText = text
         resultList.clear()
 
-        if (text!!.isNotEmpty()) setupAdapters(movieList)
+        if (searchText.isNullOrEmpty()) setupAdapters(movieList)
         else {
             movieList.forEach {
                 if (it.title.contains(text, ignoreCase = false)) resultList.add(it)
             }
-            setupAdapters(resultList)
+            if (resultList.isNullOrEmpty()) setupAdapters(movieList)
+            else setupAdapters(resultList)
         }
         setupViews()
     }
 
+    private fun initListener() {
+        searchBTN.setOnClickListener {
+            onSearchSubmit()
+        }
+    }
     private fun getMoviesList() {
         val list = App.movieRepository.getMovieList()
 
         if (list.isNullOrEmpty())
             showMessage("Filmes não foram carregados,verifique sua conexão.")
         else movieList = list
-
     }
-
 
     private fun setupAdapters(list: List<Movie>) {
         adapter =
@@ -75,31 +86,26 @@ class SearchFragment : Fragment(), Search.View, MovieAdapter.Listener {
     }
 
     override fun getViewContext(): Context? {
-        return context
+        return this
     }
 
     override fun openMovieFragment(movie: Movie, itemView: View) {
         val options = ActivityOptions.makeSceneTransitionAnimation(
-            activity,
+            this,
             Pair(itemView, TheMovieActivity.TRANSITION_IMAGE)
         )
 
-        val intent = Intent(context, TheMovieActivity::class.java).apply {
+        val intent = Intent(this, TheMovieActivity::class.java).apply {
             putExtra("imageTitle", movie.title)
             putExtra("imagePosterPath", (movie.poster_path))
             putExtra("imageOverview", (movie.overview))
         }
-        activity?.startActivity(intent, options.toBundle())
+        startActivity(intent, options.toBundle())
         adapter?.notifyDataSetChanged()
     }
+//
+//    companion object {
+//        const val SEARCH_ARG = "search_arg"
+//    }
 
-    companion object {
-        const val SEARCHTEXT_ARG = "searchText_arg"
-        fun newInstance(searchText: String) = SearchFragment().apply {
-            arguments = Bundle().apply {
-                putString(SEARCHTEXT_ARG, searchText)
-            }
-            return SearchFragment()
-        }
-    }
 }
